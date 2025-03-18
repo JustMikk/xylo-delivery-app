b9d3142dcf33773cb3c626f3f888c834f9853e87
Mikiyas Hailegebriel <mikiyasbutu@gmail.com>
2025-03-18 17:42:02 +0300

    Update TypeScript configuration to include module resolution and additional type definitions

---
+++ constants/icons.ts
@@ -0,0 +1,13 @@
+import helpIcon from '../assets/icons/help.png';
+import myOrdersIcon from '../assets/icons/my-orders.png';
+import menuIcon from '../assets/icons/menu.png';
+import favouritesIcon from '../assets/icons/favourites.png';
+import homeIcon from '../assets/icons/home.png';
+
+export const icons = {
+  help: helpIcon,
+  myOrders: myOrdersIcon,
+  menu: menuIcon,
+  favourites: favouritesIcon,
+  home: homeIcon,
+} as const;
---
+++ constants/images.ts
@@ -0,0 +1,21 @@
+import logo from '../assets/images/logo.png';
+import adaptiveIcon from '../assets/images/adaptive-icon.png';
+import favicon from '../assets/images/favicon.png';
+import icon from '../assets/images/icon.png';
+import partialReactLogo from '../assets/images/partial-react-logo.png';
+import reactLogo from '../assets/images/react-logo.png';
+import reactLogo2x from '../assets/images/react-logo@2x.png';
+import reactLogo3x from '../assets/images/react-logo@3x.png';
+import splashIcon from '../assets/images/splash-icon.png';
+
+export const images = {
+  logo,
+  adaptiveIcon,
+  favicon,
+  icon,
+  partialReactLogo,
+  reactLogo,
+  reactLogo2x,
+  reactLogo3x,
+  splashIcon,
+} as const;
